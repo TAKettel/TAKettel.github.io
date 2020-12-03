1b63bd51fcc552e097b9a8e401ee8f7e3eb654a8
Trevor <takettel@hotmail.com>
2020-12-02 19:41:50 -0600

    updated JSON

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,31 @@
-$(function(){
-    for (let i = 0; i < 6; i++) {
-    wedDevThumb(i);
-    }
+
+
+$(function() {
+    webDevThumbs();
     for (let j = 6; j < 18; j++) {
     chaDesThumb(j);
     }
 });
 
-function wedDevThumb(i) {
+function webDevThumbs() {
+let requestURL = 'https://takettel.github.io/assets/webDevThumbs.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseText = 'json';
+request.send();
+request.onload = function() {
+    let webDevThumbs = request.response;
+    webDevThumbs = JSON.stringify(webDevThumbs);
+    webDevThumbs = JSON.parse(webDevThumbs);
+    console.log(webDevThumbs[1].image)
+    for (let i = 0; i < webDevThumbs.length; i++) {
+        webDevThumb(i);
+        }    
+
+}};
+
+
+function webDevThumb(i) {
     let thumb = $("<div class='card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2' data-toggle='modal' data-target='.webdevDetails'>").text("Website " + i);
     $("#webDevThumbnails").append(thumb)
 };
