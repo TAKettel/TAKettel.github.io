6024dc183e4c174110a09ea3d95d46e291c615a2
Trevor <takettel@hotmail.com>
2020-12-09 17:33:30 -0600

    updates to portfolio

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,13 +31,12 @@ function chaDesThumbs() {
         });
 };
 
-
 function webDevThumb(data) {
     console.log(data.length + " items found");
     for (let i = 0; i < data.length; i++) {
     let thumb = $('<div/>', {
-        'class': 'card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 wdThumbnail',
-        'id':  data[i].id,
+        'class': 'card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 thumbnail webDev',
+        'id':  i,
         'data-toggle': 'modal',
         'data-target' : '.webdevDetails',
         }).text("Website: " + data[i].title);
@@ -48,10 +47,36 @@ function chaDesThumb(data) {
     console.log(data.length + " items found");
     for (let j = 0; j < data.length; j++) {
     let thumb = $('<div/>', {
-        'class': 'card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 cdThumbnail',
-        'id':  data[j].id,
+        'class': 'card col-6 col-md-4 col-lg-3 col-xl-2 thumbnail chaDes',
+        'id':  j,
         'data-toggle': 'modal',
         'data-target' : '.chaDesDetails',
-        }).text("Character: " + data[j].title);
+        }).html("<img src='" + data[j].thumbnail + "' alt ='" + data[j].title + "' class='thumbImg'>");
     $("#chaDesThumbnails").append(thumb)
-}};
+    };
+    thumbButt();
+};
+
+function thumbButt() {
+    $(document).on('contextmenu', 'img', function() {
+        return false;
+    })
+    $(".thumbnail").on("click", function() {
+        let id = this.id;
+        if ($(this).hasClass("webDev")) {
+        $.getJSON( "https://takettel.github.io/assets/webDevThumbs.json", function(data) {
+        }).done(function(data) {    
+            $(".webdevDetails").find(".titleBox").text(data[id].title);
+            $(".webdevDetails").find(".previewImage").html("<img src='" + data[id].image + "' alt ='" + data[id].title + "' class='fullImage'>");
+            $(".webdevDetails").find(".descriptBox").text(data[id].description);
+            })
+        } else {
+        $.getJSON( "https://takettel.github.io/assets/chaDesThumbs.json", function(data) {
+        }).done(function(data) {    
+            $(".chaDesDetails").find(".titleBox").text(data[id].title);
+            $(".chaDesDetails").find(".previewImage").html("<img src='" + data[id].image + "' alt ='" + data[id].title + "' class='fullImage'>");
+            $(".chaDesDetails").find(".descriptBox").text(data[id].description);
+            })
+        }
+    })
+}
