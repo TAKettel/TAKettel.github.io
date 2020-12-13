925b510305fce87485d2714bc217a933b17e9962
Trevor <takettel@hotmail.com>
2020-12-12 18:16:40 -0600

    updates to modal presentation

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,34 +5,29 @@ $(function() {
 
 function webDevThumbs() {
     let jqxhr = $.getJSON( "https://takettel.github.io/assets/webDevThumbs.json", function(data) {
-        console.log( "JSON object obtained" );
         })
         .fail(function() {
         console.log( "error" );
         })
         .done(function(data) {
         const webDevThumbs = data
-        console.log(webDevThumbs);
         webDevThumb(webDevThumbs)
         });
 };
 
 function chaDesThumbs() {
     let jqxhr = $.getJSON( "https://takettel.github.io/assets/chaDesThumbs.json", function(data) {
-        console.log( "JSON object obtained" );
         })
         .fail(function() {
         console.log( "error" );
         })
         .done(function(data) {
         const chaDesThumbs = data
-        console.log(chaDesThumbs);
         chaDesThumb(chaDesThumbs)
         });
 };
 
 function webDevThumb(data) {
-    console.log(data.length + " items found");
     for (let i = 0; i < data.length; i++) {
     let thumb = $('<div/>', {
         'class': 'card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 thumbnail webDev',
@@ -44,7 +39,6 @@ function webDevThumb(data) {
 }};
 
 function chaDesThumb(data) {
-    console.log(data.length + " items found");
     for (let j = 0; j < data.length; j++) {
     let thumb = $('<div/>', {
         'class': 'card col-6 col-md-4 col-lg-3 col-xl-2 thumbnail chaDes',
@@ -64,14 +58,15 @@ function thumbButt() {
     $(".thumbnail").on("click", function() {
         let id = this.id;
         if ($(this).hasClass("webDev")) {
-        $.getJSON( "https://takettel.github.io/assets/webDevThumbs.json", function(data) {
+        $.getJSON( "https://takettel.github.io/assets/webDevThumbs.json", function() {
         }).done(function(data) {    
             $(".webdevDetails").find(".titleBox").text(data[id].title);
             $(".webdevDetails").find(".previewImage").html("<img src='" + data[id].image + "' alt ='" + data[id].title + "' class='fullImage'>");
-            $(".webdevDetails").find(".descriptBox").text(data[id].description);
+            $(".webdevDetails").find(".descriptBox").html(
+                "<row>" + data[id].description + "</row><row>Built with " + data[id].tools + ".</row><row><div class='col'><a href='" + data[id].deployLink + "' target='_blank'>See it in action.</a></div><div class='col'><a href='" + data[id].gitLink + "' target='_blank'>Find the repository on GitHub.</a></row></div>");
             })
         } else {
-        $.getJSON( "https://takettel.github.io/assets/chaDesThumbs.json", function(data) {
+        $.getJSON( "https://takettel.github.io/assets/chaDesThumbs.json", function() {
         }).done(function(data) {    
             $(".chaDesDetails").find(".titleBox").text(data[id].title);
             $(".chaDesDetails").find(".previewImage").html("<img src='" + data[id].image + "' alt ='" + data[id].title + "' class='fullImage'>");
