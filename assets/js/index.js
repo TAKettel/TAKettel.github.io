8ac01301bac6d6207294f22632afe643e902be68
Trevor <takettel@hotmail.com>
2020-12-12 18:41:51 -0600

    webDev images uploaded

--- assets/js/index.js
+++ assets/js/index.js
@@ -63,7 +63,7 @@ function thumbButt() {
             $(".webdevDetails").find(".titleBox").text(data[id].title);
             $(".webdevDetails").find(".previewImage").html("<img src='" + data[id].image + "' alt ='" + data[id].title + "' class='fullImage'>");
             $(".webdevDetails").find(".descriptBox").html(
-                "<row>" + data[id].description + "</row><row>Built with " + data[id].tools + ".</row><row><div class='col'><a href='" + data[id].deployLink + "' target='_blank'>See it in action.</a></div><div class='col'><a href='" + data[id].gitLink + "' target='_blank'>Find the repository on GitHub.</a></row></div>");
+                "<row>" + data[id].description + "</row><row>Built with " + data[id].tools + "</row><row><div class='col'><a href='" + data[id].deployLink + "' target='_blank'>See it in action.</a></div><div class='col'><a href='" + data[id].gitLink + "' target='_blank'>Find the repository on GitHub.</a></row></div>");
             })
         } else {
         $.getJSON( "https://takettel.github.io/assets/chaDesThumbs.json", function() {
